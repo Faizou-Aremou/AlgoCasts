9f8ac6abc6d2a982d51e9b6c8ae186bb91618c3b
Faizou <faizouaremou@gmail.com>
2024-07-07 16:09:13 +0200

    matrix with steven solution

--- exercises/matrix/index.ts
+++ exercises/matrix/index.ts
@@ -15,38 +15,67 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 // matrix(5)
-       [[1,2,3,4,5],
-        [16,17,18,19,6], // recursive + 1 vers l'avant dernier, recursive + 1 vers le bas jusqu'à l'avant derniere ligne,
-        [15,24,25,20,7],
-        [14,23,22,21,8], // 
-        [13,12,11,10,9],
-       ]
+[
+  [1, 2, 3, 4, 5],
+  [16, 17, 18, 19, 6], // recursive + 1 vers l'avant dernier, recursive + 1 vers le bas jusqu'à l'avant derniere ligne,
+  [15, 24, 25, 20, 7],
+  [14, 23, 22, 21, 8], //
+  [13, 12, 11, 10, 9],
+];
 
 /**
- * matrix(0,t) = []
- * matrix(n+1,t) = soit <pR> = createMRow(n,t);
- *                      createInsideRow(t,pR)
- *                              
- * 
- * 
- * createInsideRow(0,pR) = []
- * createInsideRow(1,[e]) =  [e+1]
- * createInsideRow(n+1,e°S) = soit <pR> = createInsideRow(n,S)
- *                                  lengh(pR)=n
- * la premiere ligne decompte le nombre; 
- * la deuxième: dernier element seconde ligne = t+1; le premier element = t+(t-1)+(t-1)+(t-2)
- *                                                                      = t+t -1 + t-1+t-2
- *                                                                      = 4t-4
- *                                                                      = 4(t-1)
- *                                                 ; recursi + 1 pour les autres (pas sur)
- * 
- * la troisième: dernier element troisième ligne = t+2; le premier element = t+(t-1)+(t-1)
- *                                                                         = (4t-4) - 1
- *                                                 ; si c'est le dernier (recursive - 1), sinon l'avant dernier elemt=l'avant dernier elemt de la ligne precedent + 1; recursivité -1 pour les autres
- * 
- * @param n 
- * @returns 
+ * spiral doesn't seems to work with recursive. Matrix is Array, recursion works better with list
+ * @param n
+ * @returns
  */
 export function matrix(n: number): number[][] {
-  return [ ];
+  const result: (null| number)[][] = emptySquareMatrix(n, n);
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while(startColumn <= endColumn && startRow <= endRow){
+    for(let i=startColumn; i<=endColumn; i++){
+      result[startRow][i]= counter;
+      counter++
+    }
+    startRow++
+    for(let i=startRow; i<=endRow; i++){
+      result[i][endColumn]= counter;
+      counter++
+    }
+    endColumn--
+    for(let i=endColumn; i>=startColumn; i--){
+      result[endRow][i]= counter;
+      counter++
+    }
+    endRow--
+    for(let i=endRow; i>=startRow; i--){
+      result[i][startColumn]= counter;
+      counter++
+    }
+    startColumn++
+  }
+  return result as number[][]
+}
+
+/**
+ * emptySquareMatrix(t,0) = []
+ * emptySquareMatrix(t,n+1) = emptyElementArray(t)° emptySquareMatrix(n)
+ * @param n
+ */
+function emptySquareMatrix(nbRow: number, nbColumns: number): null[][] {
+  if (nbColumns === 0) {
+    return [];
+  }
+  return [emptyElementArray(nbRow), ...emptySquareMatrix(nbRow, nbColumns - 1)];
+}
+
+function emptyElementArray(nbRow: number): null[] {
+  if (nbRow === 0) {
+    return [];
+  }
+  return [null, ...emptyElementArray(nbRow - 1)];
 }
